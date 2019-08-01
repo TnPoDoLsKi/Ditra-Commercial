c8a4fcba317cc59b79cf5b48fef0c3d9230004eb
ousema <masmoudiousema9@gmail.com>
2019-08-01 09:57:19 +0100

    test

---
+++ src/main/java/com/ditrasystems/comspringboot/ComspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ditrasystems.comspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComspringbootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ComspringbootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ditrasystems/comspringboot/ComspringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ditrasystems.comspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComspringbootApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
